92438faf7264dff43ff19faea6b3465b7ec8490e
Krzysztof Kąkol <krix38@gmail.com>
2017-08-15 14:25:03 +0200

    main class refactoring

--- src/main/java/pl/krix/generator/Main.java
+++ src/main/java/pl/krix/generator/Main.java
@@ -1,8 +1,12 @@
 package pl.krix.generator;
 
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.krix.generator.api.service.RaportGenerationService;
 import pl.krix.generator.exception.RaportGenerationException;
 import pl.krix.generator.impl.service.RaportGenerationServiceImpl;
+
 
 /**
  * Created by krix on 04.08.2017.
@@ -11,13 +15,16 @@ public class Main {
 
     private static RaportGenerationService raportGenerationService;
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     //TODO: print usage if wrong args
     public static void main(String[] args){
+        BasicConfigurator.configure();
         try {
             raportGenerationService = new RaportGenerationServiceImpl();
             raportGenerationService.generate(args[0]);
         } catch (RaportGenerationException e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }
     }
 }

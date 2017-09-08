c9a3708e333bbc751f03d5c9007eb320359ebfaf
Krzysztof Kąkol <krix38@gmail.com>
2017-09-08 12:23:28 +0200

    example mapping configuration -adding csv input fields

--- src/main/java/pl/krix/generator/domain/csv/Csv.java
+++ src/main/java/pl/krix/generator/domain/csv/Csv.java
@@ -32,4 +32,53 @@ public class Csv {
     @CsvField(pos = 6)
     private String accountHolderLegalAddressType;
 
+    @CsvField(pos = 7)
+    private String accountHolderCountryCode;
+
+    @CsvField(pos = 8)
+    private String accountHolderAddressFree;
+
+    @CsvField(pos = 9)
+    private String accountHolderResCountryCode;
+
+    @CsvField(pos = 10)
+    private String controllingPersonResCountryCode;
+
+    @CsvField(pos = 11)
+    private String controllingPersonTinCountryCode;
+
+    @CsvField(pos = 12)
+    private String controllingPersonTinIssuedByCountryCode;
+
+    @CsvField(pos = 13)
+    private String controllingPersonName;
+
+    @CsvField(pos = 14)
+    private String controllingPersonLastname;
+
+    @CsvField(pos = 15)
+    private String controllingPersonLegalAddressType;
+
+    @CsvField(pos = 16)
+    private String controllingPersonCountryCode;
+
+    @CsvField(pos = 17)
+    private String controllingPersonAddressFree;
+
+    @CsvField(pos = 18)
+    private String accountBalance;
+
+    @CsvField(pos = 19)
+    private String accountBalanceCurrCode;
+
+
+
+
+
+
+
+
+
+
+
 }
